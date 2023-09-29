eb570e5549cf0e66773a1be920dddea4915884a2
Sergio <sergiorobayorr@gmail.com>
2023-09-28 21:13:48 -0500

    Authorization: protecting routes

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,19 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isLoading: isLoginIn } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      console.log(user);
+      queryClient.setQueriesData(["user", user]);
       navigate("/dashboard");
     },
     onError: (err) => {
-      console.log("ERROR", err);
+      console.error("ERROR", err);
       toast.error("Provided email or password are incorrect");
     },
   });
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -8,6 +8,18 @@ export async function login({ email, password }) {
 
   if (error) throw new Error(error.message);
 
-  console.log(data);
   return data;
+}
+
+export async function getCurrentuser() {
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  console.log(data);
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
 }
