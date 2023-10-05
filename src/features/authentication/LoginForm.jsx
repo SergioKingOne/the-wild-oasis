5d1f805ad119eae37f8706d377c12d52dae11588
Sergio <sergiorobayorr@gmail.com>
2023-10-05 08:40:43 -0500

    Fixing small bugs

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("sergiostest2312@gmail.com");
-  const [password, setPassword] = useState("wildoasis6!");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoginIn } = useLogin();
 
   function handleSubmit(e) {
